1c437a79925f12ab7cde54fd533fce1e4674e0bd
Katie Abraham <katie@katies-air.lan>
2021-01-01 15:47:13 -0500

    working on audio player

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render () {
         return (
-            <div>
-                <p>contact info will go here.</p>
+            <div className="homescreen">
+                <div>
+                    <p>contact info will go here.</p>
+                </div>
             </div>
         )
     }
--- src/components/Media.js
+++ src/components/Media.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
 class Media extends Component {
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.selectedTrack !== prevState.selectedTrack) {
+            let track;
+            switch(this.state.selectedTrack) {
+                case "Sein wir wieder Gut":
+                    track = seinWirWiederGut
+                break;
+                case "I Am Not My Own":
+                    track = iAmNotMyOwn
+                    break;
+                    default:
+                    break;
+            }
+            if(track) {
+                this.player.src = track;
+                this.player.play()
+            }
+        }
+    }
+
     render () {
+        const list = [{id: 1, title: "Sein wir wieder Gut"}, {id: 2, title: "I am not my own"}].map(item => {
+            return (
+                <button>
+                    <li key={item.id}
+                    onClick={() => this.setState({selectedTrack: item.title })}>
+                        {item.title}
+                    </li>
+                </button>
+            );
+        });
+
         return (
-            <div>
-                <p>This is where recordings will go.</p>
+            <div className="homescreen">
+                <div className="homeContainer">
+                    <ul>{list}</ul>
+                    <audio ref={ref => this.player = ref}></audio>
+                </div>
             </div>
         )
     }

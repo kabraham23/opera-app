ad96c04fb34b76029f4004a646070e141f1cb24b
Katie Abraham <katie.t.abraham@gmail.com>
2021-01-03 16:44:05 -0500

    contact form working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+const Mailto = ({ email, subject = '', body = '', children }) => {
+    let params = subject || body ? '?' : '';
+    if (subject) params += `subject=${encodeURIComponent(subject)}`;
+    if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+
+    return <a href={`mailto:${email}${params}`}>{children}</a>
+};
+
 class Contact extends Component {
     render () {
         return (
             <div className="homescreen">
                 <div>
-                    <p>contact info will go here.</p>
+                    <h3>I'm not singing professionally anymore, but if you need a website for yourself, please let me know!</h3>
+                    <Mailto email="katie.t.abraham@gmail.com" obfuscate={true}>
+                        Email me!
+                    </Mailto>
                 </div>
             </div>
         )
